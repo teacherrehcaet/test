e58abf332a3a1686753dd86c161960a36287758d
teacher <777mongol@gmail.com>
2020-04-09 06:23:01 +0300

    hook test

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -2,7 +2,6 @@ import org.testng.annotations.Test;
 
 public class FirstTest {
 
-
     @Test
     public void studentTest(){
         System.out.print("Hello student!\n");
@@ -22,13 +21,9 @@ public class FirstTest {
         System.out.print(methodName + " for teacher & student\n");
     }
 
-
-
-    @Test
-    public void paramTest(){
-        String teacherName = System.getProperty("teacherName");
-        System.out.print(teacherName + " , hi! \n");
-    }
-
-
+//    @Test
+//    public void paramTest(){
+//        String teacherName = System.getProperty("teacherName");
+//        System.out.print(teacherName + " , hi! \n");
+//    }
 }
